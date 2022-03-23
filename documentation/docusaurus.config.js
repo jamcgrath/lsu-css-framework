f880bb1cd91b458a1a496feb255814a59737b788
James McGrath <james@jamesmcgrath.net>
2022-03-24 08:44:35 +0900

    add github link

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
 					},
 					{ to: "/blog", label: "Blog", position: "left" },
 					{
-						href: "https://github.com/facebook/docusaurus",
+						href: "https://github.com/jamcgrath/css-framework",
 						label: "GitHub",
 						position: "right",
 					},
