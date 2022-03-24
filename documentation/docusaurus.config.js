80c03c5e5dd55dc1c53b3aff38da305dacc2b1ee
James McGrath <james@jamesmcgrath.net>
2022-03-24 09:23:18 +0900

    another try to deploy

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -34,10 +34,7 @@ const config = {
 						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
 				},
 				theme: {
-					customCss: [
-						require.resolve("./src/css/styles.css"),
-						require.resolve("./src/css/custom.css"),
-					],
+					customCss: [require.resolve("./src/css/custom.css")],
 				},
 			}),
 		],
