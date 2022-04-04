c7a39493182cb216515f98f4b153e9a00dfd1999
James <jamcgrath@users.noreply.github.com>
2022-04-04 09:05:04 +0900

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
 				docs: {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
-					editUrl: "https://github.com/jamcgrath/css-framework/",
+					editUrl: "https://github.com/jamcgrath/css-framework/tree/main/documentation/docs",
 				},
 				// blog: {
 				// 	showReadingTime: true,
